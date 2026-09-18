f2aae7827d322acdbf835579f3ba14fc9b97b448
62294 <62294@etu.he2b.be>
2026-09-18 11:06:27 +0200

    remove router because not needed for this simple app

---
+++ tic-tac-toe/src/app/square-component/square-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-square-component',
+  styleUrl: './square-component.scss',
+  templateUrl: './square-component.html',
+})
+export class SquareComponent {}
