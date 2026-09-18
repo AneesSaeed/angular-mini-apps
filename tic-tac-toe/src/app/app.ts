3f54388e082fcd0d34acb45aeaac23c6baa933e2
62294 <62294@etu.he2b.be>
2026-09-18 17:21:43 +0200

    add header with a logo and improve styling

--- tic-tac-toe/src/app/app.ts
+++ tic-tac-toe/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { BoardComponent } from './board-component/board-component';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
-  imports: [BoardComponent]
+  imports: [Header, BoardComponent]
 })
 export class App {
   protected readonly title = signal('tic-tac-toe');
